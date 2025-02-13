578d4b843a25a9deb22b1fac2fdc599ecc66e8fa
anchvo <ac.voretzsch@gmx.de>
2025-02-13 15:56:09 +0100

    Add Patients model

--- doctor_appointments/models.py
+++ doctor_appointments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Patients(models.Model):
+    name = models.CharField(max_length=400)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=300)
+    phone = models.IntegerField()
+    password = models.CharField()
+    preferred_contact = models.BooleanField()
+    additional_info = models.TextField()
