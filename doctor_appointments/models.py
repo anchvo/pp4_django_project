3b0535af692dfbf5be47708d90faedbbd84865ff
anchvo <ac.voretzsch@gmx.de>
2025-02-13 16:37:38 +0100

    Delete Patients model with wrong spelling and create new Patient model

--- doctor_appointments/models.py
+++ doctor_appointments/models.py
@@ -3,11 +3,10 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Patients(models.Model):
-    name = models.CharField(max_length=400)
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=300)
-    phone = models.IntegerField()
-    password = models.CharField()
-    preferred_contact = models.BooleanField()
-    additional_info = models.TextField()
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self): 
+        return self.user.username
+
+
