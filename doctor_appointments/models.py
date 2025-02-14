15aa74b9399bfc8990d8c33b71a1709dea1619f6
anchvo <ac.voretzsch@gmx.de>
2025-02-14 21:18:27 +0100

    Add role, location, features and specialisation models

--- doctor_appointments/models.py
+++ doctor_appointments/models.py
@@ -11,7 +11,7 @@ class Patient(models.Model):
     preferred_contact = models.BooleanField(default=None)
 
     def __str__(self):
-        return self.user.username
+        return self.full_name
 
 
 class Doctor(models.Model):
@@ -21,21 +21,52 @@ class Doctor(models.Model):
     practice_name = models.CharField(max_length=400, blank=True)
     email = models.EmailField(max_length=254, blank=True)
     phone_number = models.IntegerField(default=None)
-    specialisation = models.CharField(max_length=200, blank=True)
+    specialisations = models.CharField(max_length=200, blank=True)
     city = models.CharField(max_length=200, blank=True)
     address = models.CharField(max_length=200, blank=True)
     features = models.BooleanField(default=None)
 
     def __str__(self):
-        return self.user.username
+        return self.full_name
+
+
+class Role(models.Model):
+    role = models.BooleanField(default="Patient")
+
+    def __str__(self):
+        return self.role
+
+
+class Location(models.Model):
+    location = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.location.city
+
+
+class Features(models.Model):
+    features = models.CharField(default=None)
+
+    def __str__(self):
+        return self.features
+
+
+class Specialisation(models.Model):
+    specialisation = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.specialisation.specialisations
 
 
 class Appointment(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor_specialisation = models.ForeignKey(
+        Specialisation, on_delete=models.CASCADE, default=1)
+    doctor_location = models.ForeignKey(Location, on_delete=models.CASCADE, default=1)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     appointment_date = models.DateTimeField()
     additional_info = models.TextField(default="Medication Plan:")
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.patient} at {self.doctor} on {self.appointment_date}"
+        return f"{self.patient.full_name} at {self.doctor.practice_name} on {self.appointment_date}"
