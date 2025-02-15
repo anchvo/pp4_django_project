87db44961520cbeb453edcc50fee7be884b7de49
anchvo <ac.voretzsch@gmx.de>
2025-02-15 13:36:25 +0100

    Adjust code to save profile form data to Location and Specialisation models

--- doctor_appointments/views.py
+++ doctor_appointments/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
-from .models import Appointment
+from .models import Appointment, Patient, Doctor, Location, Specialisation
 from .forms import PatientProfileForm, DoctorProfileForm
 
 
@@ -60,7 +60,20 @@ def view_doctor_profile_form(request):
             doctor = form.save(commit=False)
             doctor.user = request.user  # Assign the logged-in user to the Doctor instance
             doctor.save()
+
+            # Save user input location to Location model 
+            location = Location(location=doctor, city=form.cleaned_data['city'])
+            location.save()
+
+            # Save user input specialisations to Specialisation model
+            specialisations = form.cleaned_data['specialisations'].split(',')  # Splitting by comma-separated list
+            for spec in specialisations:
+                spec = spec.strip()  # Clean extra spaces
+                specialisation = Specialisation(specialisation=doctor, specialisations=spec)
+                specialisation.save()
+
             return HttpResponse('Your Profile was set up correctly')
+        
         else:
             print(form.errors)  # Check form errors
             # If the form is invalid, render form with errors
